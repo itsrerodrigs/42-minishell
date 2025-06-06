62fd8b35e77059a1229d7a35a08d2e3cc4b1fb9d
Marieli Mariano Goncalves Dias <mmariano@c1r2p3.42sp.org.br>
2025-06-06 15:18:04 -0300

    fix cd ~

--- inc/builtins.h
+++ inc/builtins.h
@@ -6,7 +6,7 @@
 /*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 19:49:10 by renrodri          #+#    #+#             */
-/*   Updated: 2025/06/05 18:23:59 by mmariano         ###   ########.fr       */
+/*   Updated: 2025/06/06 15:14:08 by mmariano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,5 +53,8 @@ int          builtin_pwd(t_shell *shell, char **args);
 int          builtin_printenv(t_shell *shell, char **args);
 builtin_func find_builtin(char *cmd);
 void	     add_or_update_env(char ***envp, const char *var);
+char    *get_cd_path_expanded(t_shell *shell, char **args);
+char    *save_and_get_oldpwd(void);
+int     execute_chdir(const char *path);
 
 #endif
