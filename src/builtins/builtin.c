819d5291674fd754cdf76ecf874325aa776d73da
Marieli Mariano Goncalves Dias <mmariano@c1r4p4.42sp.org.br>
2025-06-10 15:39:51 -0300

    redir out > working

--- src/builtins/builtin.c
+++ src/builtins/builtin.c
@@ -6,7 +6,7 @@
 /*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 18:29:56 by mmariano          #+#    #+#             */
-/*   Updated: 2025/06/05 18:31:40 by mmariano         ###   ########.fr       */
+/*   Updated: 2025/06/10 15:34:38 by mmariano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,33 @@
  * params args - an array of strings representing command-line arguments
  * return 0 on success or 1 if the environment variablws are not accessible
 */
-int	ft_env(char **args)
-{
-	extern char	**environ;
-	int	i;
+// int	ft_env(char **args)
+// {
+// 	extern char	**environ;
+// 	int	i;
 
-	i = 0;
-	(void)args;
-	if (!environ)
-		return(1);
-	while (environ[i])
-	{
-		p("%s\n", environ[i]);
-		i++;
-	}
-	return(0);
+// 	i = 0;
+// 	(void)args;
+// 	if (!environ)
+// 		return(1);
+// 	while (environ[i])
+// 	{
+// 		p("%s\n", environ[i]);
+// 		i++;
+// 	}
+// 	return(0);
+// }
+
+bool is_builtin_parent_executable(t_command *cmd)
+{
+	if (!cmd || !cmd->args || !cmd->args[0])
+		return (false);
+	if (ft_strncmp(cmd->args[0], "cd", ft_strlen(cmd->args[0]) +1) ||
+		ft_strncmp(cmd->args[0], "exit", ft_strlen(cmd->args[0]) + 1) ||
+		ft_strncmp(cmd->args[0], "export", ft_strlen(cmd->args[0]) + 1) ||
+		ft_strncmp(cmd->args[0], "unset", ft_strlen(cmd->args[0]) + 1))
+		{
+			return (!cmd->is_pipe && cmd->prev_pipe_read_fd == -1);
+		}
+		return (false);
 }
--- src/builtins/exec_builtin.c
+++ src/builtins/exec_builtin.c
@@ -6,11 +6,11 @@
 /*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:36:07 by renrodri          #+#    #+#             */
-/*   Updated: 2025/06/10 15:27:02 by mmariano         ###   ########.fr       */
+/*   Updated: 2025/06/10 15:37:08 by mmariano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/minishell.h"
+#include "minishell.h"
 
 static t_builtin	*get_builtins(void)
 {
@@ -46,85 +46,87 @@ builtin_func	find_builtin(char *cmd)
 	return (NULL);
 }
 
-bool is_builtin_parent_executable(t_command *cmd)
+
+/*
+ * @brief Saves the original STDIN and STDOUT file descriptors.
+ * @param orig_stdin Pointer to store original STDIN FD.
+ * @param orig_stdout Pointer to store original STDOUT FD.
+ * @return 0 on success, 1 on failure (e.g., dup failed).
+ */
+static int s_save_original_fds(int *orig_stdin, int *orig_stdout)
 {
-	if (!cmd || !cmd->args || !cmd->args[0])
-		return (false);
-	if (ft_strncmp(cmd->args[0], "cd", ft_strlen(cmd->args[0]) +1) ||
-		ft_strncmp(cmd->args[0], "exit", ft_strlen(cmd->args[0]) + 1) ||
-		ft_strncmp(cmd->args[0], "export", ft_strlen(cmd->args[0]) + 1) ||
-		ft_strncmp(cmd->args[0], "unset", ft_strlen(cmd->args[0]) + 1))
-		{
-			return (!cmd->is_pipe && cmd->prev_pipe_read_fd == -1);
-		}
-		return (false);
+    *orig_stdin = dup(STDIN_FILENO);
+    *orig_stdout = dup(STDOUT_FILENO);
+    if (*orig_stdin == -1 || *orig_stdout == -1) {
+        perror("minishell: dup failed in builtin setup");
+        if (*orig_stdin != -1) close(*orig_stdin);
+        if (*orig_stdout != -1) close(*orig_stdout);
+        return (1);
+    }
+    return (0);
 }
 
-// int	exec_builtin(t_shell *shell)
-// {
-// 	char		*cmd;
-// 	int		builtin_return_status;
-// 	builtin_func	func;
+/*
+ * @brief Restores the original STDIN and STDOUT file descriptors and closes saved FDs.
+ * @param orig_stdin Original STDIN FD.
+ * @param orig_stdout Original STDOUT FD.
+ */
+static void s_restore_fds(int orig_stdin, int orig_stdout)
+{
+    dup2(orig_stdin, STDIN_FILENO);
+    dup2(orig_stdout, STDOUT_FILENO);
+    close(orig_stdin);
+    close(orig_stdout);
+}
 
-// 	if (!shell || !shell->current_cmd || !shell->current_cmd->args[0])
-// 		return (0);
-// 	cmd = shell->current_cmd->args[0];
-// 	func = find_builtin(cmd);
-// 	if (func)
-// 	{
-// 		builtin_return_status = func(shell, shell->current_cmd->args);
-// 		shell->exit_status = builtin_return_status;
-// 		return (1);
-// 	}
-// 	return (0);
-// }
+/*
+ * @brief Applies redirections and executes the built-in function.
+ * Sets shell->exit_status based on builtin's return.
+ * @param shell The shell context.
+ * @param func The builtin function pointer to execute.
+ * @return 1 if builtin successfully executed (regardless of its internal exit status).
+ * -1 if redirection application failed (exit status is set in shell).
+ */
+static int s_perform_builtin_io_and_exec(t_shell *shell, builtin_func func)
+{
+    int builtin_exit_status;
 
+    if (apply_redirections(shell->current_cmd) != 0) 
+    {
+        shell->exit_status = 1; 
+        return (-1);
+    }
+    builtin_exit_status = func(shell, shell->current_cmd->args);
+    shell->exit_status = builtin_exit_status; 
+    return (1);
+}
 
+/*
+ * @brief Executes a built-in command with proper I/O redirection.
+ * @param shell The shell context.
+ * @return 1 if a built-in was found and executed (regardless of its success/failure),
+ * 0 if no built-in was found or if initial setup failed.
+ */
 int exec_builtin(t_shell *shell)
 {
     char            *cmd;
     builtin_func    func;
-    int             builtin_return_status;
-    int             original_stdin = dup(STDIN_FILENO);  
-    int             original_stdout = dup(STDOUT_FILENO);
-
-    if (original_stdin == -1 || original_stdout == -1) 
-	{
-        perror("minishell: dup failed in builtin redirection setup");
-        if (original_stdin != -1) close(original_stdin);
-        if (original_stdout != -1) close(original_stdout);
-        return (0); 
-    }
+    int             orig_stdin;
+    int             orig_stdout;
+    int             exec_result;
 
     if (!shell || !shell->current_cmd || !shell->current_cmd->args || !shell->current_cmd->args[0])
-    {
-        close(original_stdin);
-        close(original_stdout);
         return (0);
-    }
-	
+
     cmd = shell->current_cmd->args[0];
     func = find_builtin(cmd);
-    if (func)
-    {
-        if (apply_redirections(shell->current_cmd) != 0) 
-		{
-            dup2(original_stdin, STDIN_FILENO);
-            dup2(original_stdout, STDOUT_FILENO);
-            close(original_stdin);
-            close(original_stdout);
-            shell->exit_status = 1; 
-            return (1);
-        }
-        builtin_return_status = func(shell, shell->current_cmd->args);
-        shell->exit_status = builtin_return_status;
-        dup2(original_stdin, STDIN_FILENO);
-        dup2(original_stdout, STDOUT_FILENO);
-        close(original_stdin);
-        close(original_stdout);
+    if (!func) 
+        return (0);
+    if (s_save_original_fds(&orig_stdin, &orig_stdout) != 0)
+        return (0);
+    exec_result = s_perform_builtin_io_and_exec(shell, func);
+    s_restore_fds(orig_stdin, orig_stdout);
+    if (exec_result == -1)
         return (1); 
-    }
-    close(original_stdin);
-    close(original_stdout);
-    return (0);
+    return (1); 
 }
