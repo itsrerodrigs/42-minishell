c84b2bd1bb38731c4db3ec1c94dca305a8a7b792
Marieli Mariano Goncalves Dias <mmariano@c1r7p2.42sp.org.br>
2025-06-11 14:43:40 -0300

    norminette: signals ok

--- src/signals/job_control.c
+++ src/signals/job_control.c
@@ -3,28 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   job_control.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: renrodri <renrodri@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 12:16:51 by mmariano          #+#    #+#             */
-/*   Updated: 2025/06/09 17:27:59 by renrodri         ###   ########.fr       */
+/*   Updated: 2025/06/11 14:41:56 by mmariano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "sig.h"
-#include "executor.h"
 
 
 /*
  ** @brief: Sets the foreground process group for a given terminal.
- ** @param fd: The file descriptor of the controlling terminal (e.g., STDIN_FILENO).
+ ** @param fd: The file descriptor of the controlling terminal (e.g.,
+	STDIN_FILENO).
  ** @param pgid: The process group ID to set as the foreground.
  ** @return: void
  */
-void set_foreground_process(int fd, pid_t pgid)
+void	set_foreground_process(int fd, pid_t pgid)
 {
-    if (ioctl(fd, TIOCSPGRP, &pgid) == -1)
-        perror("minishell: ioctl TIOCSPGRP failed");
+	if (ioctl(fd, TIOCSPGRP, &pgid) == -1)
+		perror("minishell: ioctl TIOCSPGRP failed");
 }
 
 /*
@@ -33,16 +32,16 @@ void set_foreground_process(int fd, pid_t pgid)
  ** @param raw_status: The raw status value returned by wait/waitpid.
  ** @return: void
  */
-void update_exit_status_from_wait_status(t_shell *shell, int raw_status)
+void	update_exit_status_from_wait_status(t_shell *shell, int raw_status)
 {
-    if (WIFEXITED(raw_status))
-        shell->exit_status = WEXITSTATUS(raw_status);
-    else if (WIFSIGNALED(raw_status))
-    {
-        shell->exit_status = 128 + WTERMSIG(raw_status);
-        if (WTERMSIG(raw_status) == SIGQUIT)
-            ft_putendl_fd("Quit (core dumped)", STDERR_FILENO);
-    }
+	if (WIFEXITED(raw_status))
+		shell->exit_status = WEXITSTATUS(raw_status);
+	else if (WIFSIGNALED(raw_status))
+	{
+		shell->exit_status = 128 + WTERMSIG(raw_status);
+		if (WTERMSIG(raw_status) == SIGQUIT)
+			ft_putendl_fd("Quit (core dumped)", STDERR_FILENO);
+	}
 }
 
 /*
@@ -51,21 +50,21 @@ void update_exit_status_from_wait_status(t_shell *shell, int raw_status)
  ** @param last_pid: The PID of the last process in the pipeline to track its status.
  ** @return: void
  */
-void pipe_wait_children(t_shell *shell, pid_t last_pid)
+void	pipe_wait_children(t_shell *shell, pid_t last_pid)
 {
-    pid_t waited_pid;
-    int status;
+	pid_t	waited_pid;
+	int		status;
 
-    if (last_pid != 1)
-    {
-        waited_pid = waitpid(last_pid, &status, 0);
-        if (waited_pid == last_pid)
-        {
-            update_exit_status_from_wait_status(shell, status);
-        }
-    }
-    while ((waited_pid = waitpid(-1, &status, WNOHANG)) > 0)
-    {
-        (void)status;
-    }
+	if (last_pid != 1)
+	{
+		waited_pid = waitpid(last_pid, &status, 0);
+		if (waited_pid == last_pid)
+		{
+			update_exit_status_from_wait_status(shell, status);
+		}
+	}
+	while ((waited_pid = waitpid(-1, &status, WNOHANG)) > 0)
+	{
+		(void)status;
+	}
 }
--- src/signals/signals_utils.c
+++ src/signals/signals_utils.c
@@ -3,98 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   signals_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marieli <marieli@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/24 18:28:53 by mmariano          #+#    #+#             */
-/*   Updated: 2025/05/12 21:57:52 by marieli          ###   ########.fr       */
+/*   Created: 2025/06/11 14:41:39 by mmariano          #+#    #+#             */
+/*   Updated: 2025/06/11 14:41:39 by mmariano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "sig.h"
 
 
-volatile sig_atomic_t g_child_running = 0;
+
+volatile sig_atomic_t	g_child_running = 0;
 
 /*
  ** @brief: Saves the current SIGINT and SIGQUIT handlers and sets them to be ignored.
  ** @param old_int: Pointer to a struct sigaction to save the old SIGINT handler.
  ** @param old_quit: Pointer to a struct sigaction to save the old SIGQUIT handler.
  */
-void save_and_ignore_signals(struct sigaction *old_int, struct  sigaction *old_quit)
+void	save_and_ignore_signals(struct sigaction *old_int,
+		struct sigaction *old_quit)
 {
-    struct sigaction sa_ignore;
+	struct sigaction sa_ignore;
 
-    sa_ignore.sa_handler = SIG_IGN;
-    sigemptyset(&sa_ignore.sa_mask);
-    sa_ignore.sa_flags = 0;
-    sigaction(SIGINT, &sa_ignore, old_int); // Save old SIGINT handler
-    sigaction(SIGQUIT, &sa_ignore, old_quit); // Save old SIGQUIT handler
+	sa_ignore.sa_handler = SIG_IGN;
+	sigemptyset(&sa_ignore.sa_mask);
+	sa_ignore.sa_flags = 0;
+	sigaction(SIGINT, &sa_ignore, old_int);   // Save old SIGINT handler
+	sigaction(SIGQUIT, &sa_ignore, old_quit); // Save old SIGQUIT handler
 }
 /*
  ** @brief: Restores previously saved SIGINT and SIGQUIT handlers.
  ** @param old_int: Pointer to a struct sigaction holding the old SIGINT handler.
  ** @param old_quit: Pointer to a struct sigaction holding the old SIGQUIT handler.
  */
-void restore_signals(struct sigaction *old_int, struct sigaction *old_quit)
+void	restore_signals(struct sigaction *old_int, struct sigaction *old_quit)
 {
-    sigaction(SIGINT, old_int, NULL); // Restore old SIGINT handler
-    sigaction(SIGQUIT, old_quit, NULL); // Restore old SIGQUIT handler
+	sigaction(SIGINT, old_int, NULL);   // Restore old SIGINT handler
+	sigaction(SIGQUIT, old_quit, NULL); // Restore old SIGQUIT handler
 }
 /*
  ** @brief: Sets SIGINT and SIGQUIT handlers to their default behavior for a child process.
  */
-void set_child_signals(void)
+void	set_child_signals(void)
 {
-    struct sigaction sa_dfl;
+	struct sigaction sa_dfl;
 
-    sa_dfl.sa_handler = SIG_DFL; // Default action for signals
-    sigemptyset(&sa_dfl.sa_mask);
-    sa_dfl.sa_flags = 0;
-    sigaction(SIGINT, &sa_dfl, NULL); // Set SIGINT to default
-    sigaction(SIGQUIT, &sa_dfl, NULL); // Set SIGQUIT to default
+	sa_dfl.sa_handler = SIG_DFL; // Default action for signals
+	sigemptyset(&sa_dfl.sa_mask);
+	sa_dfl.sa_flags = 0;
+	sigaction(SIGINT, &sa_dfl, NULL);  // Set SIGINT to default
+	sigaction(SIGQUIT, &sa_dfl, NULL); // Set SIGQUIT to default
 }
 
 /*
  ** @brief: Handles the SIGINT (Ctrl+C) signal.
  ** @param signum: The signal number (SIGINT).
  */
-void sigint_handler(int signum)
+void	sigint_handler(int signum)
 {
-    (void)signum;
-    
-    if (g_child_running == 1)
-        write(1, "\n", 1);
-    else
-    {
-        write(1, "\n", 1);
-        rl_on_new_line();
-        rl_replace_line("", 0);
-        rl_redisplay();
-    }
+	(void)signum;
+
+	if (g_child_running == 1)
+		write(1, "\n", 1);
+	else
+	{
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
 /*
  ** @brief: Sets up initial signal handling for the minishell.
  */
-void setup_signal_handling(void)
+void	setup_signal_handling(void)
 {
-    struct sigaction sa_int; 
-    struct sigaction sa_quit;
+	struct sigaction sa_int;
+	struct sigaction sa_quit;
 
-    sa_int.sa_handler = sigint_handler;
-    sigemptyset(&sa_int.sa_mask);
-    sa_int.sa_flags = SA_RESTART;
-    if (sigaction(SIGINT, &sa_int, NULL) == -1)
-    {
-        perror("sigaction for SIGINT failed");
-        exit(EXIT_FAILURE);
-    }
-    sa_quit.sa_handler = SIG_IGN; /* Ignore ctrl+\ */
-    sigemptyset(&sa_quit.sa_mask);
-    sa_quit.sa_flags = 0;
-    if(sigaction(SIGQUIT, &sa_quit, NULL) == -1)
-    {
-        perror("sigaction for SIGQUIT failed");
-        exit(EXIT_FAILURE);
-    }
+	sa_int.sa_handler = sigint_handler;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = SA_RESTART;
+	if (sigaction(SIGINT, &sa_int, NULL) == -1)
+	{
+		perror("sigaction for SIGINT failed");
+		exit(EXIT_FAILURE);
+	}
+	sa_quit.sa_handler = SIG_IGN; /* Ignore ctrl+\ */
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = 0;
+	if (sigaction(SIGQUIT, &sa_quit, NULL) == -1)
+	{
+		perror("sigaction for SIGQUIT failed");
+		exit(EXIT_FAILURE);
+	}
 }
