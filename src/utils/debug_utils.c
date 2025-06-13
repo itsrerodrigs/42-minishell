6a420a14a870e9f91704d05078f6b4bf694c7215
Marieli Mariano Goncalves Dias <mmariano@c2r12p3.42sp.org.br>
2025-06-13 16:36:06 -0300

    norminette: utils OK

--- src/utils/debug_utils.c
+++ src/utils/debug_utils.c
@@ -2,45 +2,38 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   debug_utils.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marieli <marieli@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: marieli <marieli@student.42.fr>            +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
 /*   Created: 2025/05/14 20:36:14 by marieli           #+#    #+#             */
 /*   Updated: 2025/05/14 22:46:38 by marieli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/minishell.h"
-#include "../inc/tokens.h"
-#include "../inc/parser.h"
+#include "minishell.h"
+#include "parser.h"
+#include "tokens.h"
 
-
-/*
- ** @brief: Prints all tokens in the linked list.
- ** @param: head - Pointer to the first token in the list.
- ** @return: None (void function).
- */
-void print_tokens(t_token *head)
+void	print_tokens(t_token *head)
 {
-    t_token *current = head;
+	t_token	*current;
 
-    while (current)
-    {
-        printf("Token: %s | Type: %d\n", current->value, current->type);
-        current = current->next;
-    }
+	current = head;
+	while (current)
+	{
+		printf("Token: %s | Type: %d\n", current->value, current->type);
+		current = current->next;
+	}
 }
 
-/*
- ** @brief: Cleans up allocated memory for input.
- ** @param input: Pointer to the input token.
- */
-void cleanup_input(t_token *input)
+void	cleanup_input(t_token *input)
 {
-    if (!input)
-        return;
-
-    if (input->value)
-        free(input->value);
-    free(input);
+	if (!input)
+		return ;
+	if (input->value)
+		free(input->value);
+	free(input);
 }
--- src/utils/memory_utils.c
+++ src/utils/memory_utils.c
@@ -2,54 +2,46 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   memory_utils.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marieli <marieli@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/24 18:28:35 by mmariano          #+#    #+#             */
-/*   Updated: 2025/05/14 22:46:38 by marieli          ###   ########.fr       */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: mmariano <mmariano@student.42sp.org.br>    +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
+/*   Created: 2025/06/13 16:30:05 by mmariano          #+#    #+#             */
+/*   Updated: 2025/06/13 16:30:05 by mmariano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/minishell.h"
-#include "../inc/tokens.h"
-#include "../inc/parser.h"
+#include "minishell.h"
+#include "parser.h"
+#include "tokens.h"
 
-/*
-** @brief: Allocates memory of size 'size' and returns a pointer to it.
-** @param: size  size of memory to allocate.
-** @return:  pointer to the allocated memory.
-*/
-void *ft_malloc(size_t size)
+void	*ft_malloc(size_t size)
 {
-    void *ptr;
-    
-    if (size == 0)
-        return(NULL);
-    ptr = malloc(size);
-    if (!ptr)
-        perror(RED "ft_malloc: allocation failed" RST);
-    return(ptr);
+	void	*ptr;
+
+	if (size == 0)
+		return (NULL);
+	ptr = malloc(size);
+	if (!ptr)
+		perror(RED "ft_malloc: allocation failed" RST);
+	return (ptr);
 }
 
-/*
-** @brief: reallocater memory with error checking
-** @paam: ptr - pointer to the previoulsy allocated memory
-** @param: size - size of memory to allocate, in bytes
-** @return: pointer to reallocated memory
-*/
-void    *ft_realloc(void *ptr, size_t size)
+void	*ft_realloc(void *ptr, size_t size)
 {
-    void *new_ptr;
+	void	*new_ptr;
 
-    if (size == 0)
-    {
-        free(ptr);
-        return (NULL);
-    }
-    if (!ptr)
-        return (ft_malloc(size));
-    new_ptr =  realloc(ptr, size);
-    if (!new_ptr)
-        perror(RED "ft_realloc: reallocation failed" RST);
-    return(new_ptr);
+	if (size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (!ptr)
+		return (ft_malloc(size));
+	new_ptr = realloc(ptr, size);
+	if (!new_ptr)
+		perror(RED "ft_realloc: reallocation failed" RST);
+	return (new_ptr);
 }
